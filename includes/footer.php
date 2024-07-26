f48cae7a90d98dbac0a62cae99135c7451da2b42
Yash-Sen <yashsen0402@gmail.com>
2024-07-26 17:42:02 +0530

    Footer Created

---
+++ includes/footer.php
@@ -0,0 +1,56 @@
+<footer id="qodef-page-footer" role="contentinfo">
+<div id="qodef-page-footer-top-area" class="qodef--has-appear">
+<div id="qodef-page-footer-top-area-inner" class="qodef-content-grid">
+<div class="qodef-grid qodef-layout--columns qodef-responsive--custom qodef-col-num--3 qodef-col-num--1024--3 qodef-col-num--880--1 qodef-col-num--680--1 qodef-alignment--center">
+<div class="qodef-grid-inner">
+<div class="qodef-grid-item">
+<div id="block-72" class="widget widget_block" data-area="qodef-footer-top-area-column-1">
+<div class="wp-block-group"><div class="wp-block-group__inner-container is-layout-constrained wp-block-group-is-layout-constrained">
+<h5 class="wp-block-heading">contact us</h5>
+<p style="font-family:jost; font-size:17px;">T. <a href="tel:123440567899">+12 344 0567899</a><br>
+M. <a href="/cdn-cgi/l/email-protection#ddbbb4b9bcb1bab29db8a5bcb0adb1b8f3beb2b0"><span class="__cf_email__" data-cfemail="20464944414c474f604558414d504c450e434f4d">[email&#160;protected]</span></a></p>
+</div></div>
+</div> </div>
+<div class="qodef-grid-item">
+<div id="block-73" class="widget widget_block" data-area="qodef-footer-top-area-column-2">
+<div class="wp-block-group"><div class="wp-block-group__inner-container is-layout-constrained wp-block-group-is-layout-constrained">
+<h5 class="wp-block-heading">address</h5>
+<a href="https://www.google.com/maps/place/P.za+della+Signoria,+12,+50122+Firenze+FI,+Italy/@43.7698405,11.254907,20z/data=!4m20!1m13!4m12!1m6!1m2!1s0x132a5400557fce11:0x77ef65e61406784a!2sP.za+della+Signoria,+12,+50122+Firenze+FI,+Italy!2m2!1d11.2552101!2d43.7698604!1m3!2m2!1d11.2552316!2d43.7698604!3e0!3m5!1s0x132a5400557fce11:0x77ef65e61406784a!8m2!3d43.7698604!4d11.2552101!16s%2Fg%2F11f3zphnxj?entry=ttu" target="_blank" rel="noopener">Piazza Della Signoria, 12 <br>21562 . Firenze . Italy</a>
+</div></div>
+</div> </div>
+<div class="qodef-grid-item">
+<div id="block-74" class="widget widget_block" data-area="qodef-footer-top-area-column-3">
+<div class="wp-block-group"><div class="wp-block-group__inner-container is-layout-constrained wp-block-group-is-layout-constrained">
+<h5 class="wp-block-heading">Opening hours</h5>
+<p>Everyday : From 12.30 To 23.00<br>Kitchen Closes At 22.00</p>
+</div></div>
+</div> </div>
+</div>
+</div>
+</div>
+</div>
+<div id="qodef-page-footer-bottom-area" class="qodef--has-appear">
+<div id="qodef-footer-logo-holder" class="qodef-content-full-width"><a href="https://fidalgo.qodeinteractive.com"><img width="376" height="124" src="https://fidalgo.qodeinteractive.com/wp-content/uploads/2023/09/logo-main.png" class="attachment-full size-full" alt="a" decoding="async" loading="lazy" srcset="https://fidalgo.qodeinteractive.com/wp-content/uploads/2023/09/logo-main.png 376w, https://fidalgo.qodeinteractive.com/wp-content/uploads/2023/09/logo-main-300x99.png 300w" sizes="(max-width: 376px) 100vw, 376px" /></a></div> <div id="qodef-page-footer-bottom-area-inner" class="qodef-content-full-width">
+<div class="qodef-grid qodef-layout--columns qodef-responsive--custom qodef-col-num--2 qodef-col-num--880--1 qodef-col-num--680--1 qodef-alignment--space-between">
+<div class="qodef-grid-inner">
+<div class="qodef-grid-item">
+<div id="fidalgo_core_social_links_group-2" class="widget widget_fidalgo_core_social_links_group" data-area="qodef-footer-bottom-area-column-1"> <div class="qodef-social-links-group">
+<span class="qodef-link-holder">
+<a itemprop="url" href="https://www.pinterest.com/qodeinteractive/" target="_blank">PINTEREST</a> </span>
+<span class="qodef-link-holder">
+<a itemprop="url" href="https://www.facebook.com/QodeInteractive/" target="_blank">FACEBOOK</a> </span>
+<span class="qodef-link-holder">
+<a itemprop="url" href="https://www.instagram.com/qodeinteractive/" target="_blank">INSTAGRAM</a> </span>
+</div>
+</div> </div>
+<div class="qodef-grid-item">
+<div id="block-7" class="widget widget_block" data-area="qodef-footer-bottom-area-column-2"><p class="qodef-copyright" style="font-family:jost; font-size:11px; font-weight:400; color:#CED8D8; letter-spacing:2px; line-height:20px;">© 2024 <a href="https://qodeinteractive.com/" target="_blank" rel="nofollow noopener">QODE INTERACTIVE</a>, ALL RIGHTS RESERVED</p></div> </div>
+</div>
+</div>
+</div>
+</div>
+</footer>
+<a id="qodef-back-to-top" href="#" class="qodef--predefined">
+<span class="qodef-back-to-top-icon">
+<span class="qodef-back-to-top-inner">Top </span><span class="qodef-back-to-top-additional-border"></span> </span>
+</a>
