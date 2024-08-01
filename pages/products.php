040fbbe3434290d54e2940f4d925d1fcfa893011
Yash-Sen <yashsen0402@gmail.com>
2024-08-01 11:15:10 +0530

    Categories displayed in product page

--- pages/products.php
+++ pages/products.php
@@ -1,5 +1,7 @@
 <?php
     require '../includes/init.php';
+    $categories = select('SELECT * FROM categories');
+    $products = select('SELECT * FROM products');
     include pathOf('includes/header.php');
     include pathOf('includes/nav.php');
 ?>
@@ -20,17 +22,6 @@
                         <div class="qodef-woo-results">
                             <p class="woocommerce-result-count">
                                 Showing 1&ndash;6 of 18 results</p>
-                            <form class="woocommerce-ordering" method="get">
-                                <select name="orderby" class="orderby" aria-label="Shop order">
-                                    <option value="menu_order" selected="selected">Default sorting</option>
-                                    <option value="popularity">Sort by popularity</option>
-                                    <option value="rating">Sort by average rating</option>
-                                    <option value="date">Sort by latest</option>
-                                    <option value="price">Sort by price: low to high</option>
-                                    <option value="price-desc">Sort by price: high to low</option>
-                                </select>
-                                <input type="hidden" name="paged" value="1" />
-                            </form>
                         </div>
                         <div
                             class="qodef-woo-product-list qodef-item-layout--info-below qodef-gutter--no qodef-vertical-gutter--no qodef-item-border--yes qodef-item-title--letter-spacing">
@@ -41,17 +32,14 @@
                                         <div class="qodef-e-content">
                                             <div class="qodef-e-top-holder">
                                                 <h5 class="qodef-woo-product-title woocommerce-loop-product__title"><a
-                                                        href="https://fidalgo.qodeinteractive.com/product/whiskey-cream/"
                                                         class="woocommerce-LoopProduct-link woocommerce-loop-product__link">Whiskey
                                                         Cream</a></h5>
                                                 <span class="price"><span
                                                         class="woocommerce-Price-amount amount"><bdi><span
                                                                 class="woocommerce-Price-currencySymbol">&#36;</span>30.00</bdi></span></span>
-                                            </div><a href="?add-to-cart=342" data-quantity="1"
-                                                class="button product_type_simple add_to_cart_button ajax_add_to_cart"
-                                                data-product_id="342" data-product_sku="1"
-                                                aria-label="Add to cart: &ldquo;Whiskey Cream&rdquo;" aria-describedby
-                                                rel="nofollow">Add to cart</a>
+                                            </div><a href="" 
+                                                class="button product_type_simple"
+                                                >Add to cart</a>
                                         </div>
                                         <div class="qodef-e-media">
                                             <div class="qodef-e-media-image"><a
@@ -276,34 +264,9 @@
                                 </li>
                             </ul>
                         </div>
-                        <nav class="woocommerce-pagination">
-                            <span aria-current="page" class="page-numbers current">1</span>
-                            <a class="page-numbers" href="https://fidalgo.qodeinteractive.com/shop/page/2/">2</a>
-                            <a class="page-numbers" href="https://fidalgo.qodeinteractive.com/shop/page/3/">3</a>
-                            <a class="next page-numbers" href="https://fidalgo.qodeinteractive.com/shop/page/2/"><svg
-                                    class="qodef-svg--pagination-arrow-right" xmlns="http://www.w3.org/2000/svg"
-                                    width="13.124" height="25.247" viewBox="0 0 13.124 25.247">
-                                    <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
-                                        <path d="m.707 24.54 11.917-11.916L.707.707" />
-                                        <path class="qodef-pagination-svg-inner"
-                                            d="m2.926 17.111 4.488-4.488-4.488-4.488" />
-                                    </g>
-                                </svg></a>
-                        </nav>
                     </div>
                     <div class="qodef-grid-item qodef-page-sidebar-section qodef-col--sidebar">
                         <aside id="qodef-page-sidebar" role="complementary">
-                            <div class="widget widget_block" data-area="shop-sidebar">
-                                <div data-block-name="woocommerce/filter-wrapper" data-filter-type="price-filter"
-                                    data-heading="Filter by price" class="wp-block-woocommerce-filter-wrapper">
-                                    <h4 class="wp-block-heading">Filter by price</h4>
-                                    <div data-block-name="woocommerce/price-filter" data-show-input-fields="false"
-                                        data-show-filter-button="true" data-heading
-                                        data-lock="{&quot;remove&quot;:true}"
-                                        class="wp-block-woocommerce-price-filter is-loading"><span aria-hidden="true"
-                                            class="wc-block-product-categories__placeholder"></span></div>
-                                </div>
-                            </div>
                             <div class="widget widget_fidalgo_core_separator" data-area="shop-sidebar">
                                 <div
                                     class="qodef-shortcode qodef-m  qodef-separator clear  qodef-border--solid qodef-show--yes qodef-show-1600--yes qodef-show-1512--yes qodef-show-1368--yes qodef-show-1200--yes qodef-show-1024--yes qodef-show-880--yes qodef-show-680--yes">
@@ -322,24 +285,12 @@
                                     style>
                                     <ul
                                         class="wc-block-product-categories-list wc-block-product-categories-list--depth-0">
-                                        <li class="wc-block-product-categories-list-item"> <a style
-                                                href="https://fidalgo.qodeinteractive.com/product-category/chocolate/"><span
-                                                    class="wc-block-product-categories-list-item__name">Chocolate</span></a><span
-                                                class="wc-block-product-categories-list-item-count"><span
-                                                    aria-hidden="true">6</span><span class="screen-reader-text">6
-                                                    products</span></span> </li>
-                                        <li class="wc-block-product-categories-list-item"> <a style
-                                                href="https://fidalgo.qodeinteractive.com/product-category/pralines/"><span
-                                                    class="wc-block-product-categories-list-item__name">Pralines</span></a><span
-                                                class="wc-block-product-categories-list-item-count"><span
-                                                    aria-hidden="true">8</span><span class="screen-reader-text">8
-                                                    products</span></span> </li>
-                                        <li class="wc-block-product-categories-list-item"> <a style
-                                                href="https://fidalgo.qodeinteractive.com/product-category/sushi/"><span
-                                                    class="wc-block-product-categories-list-item__name">Sushi</span></a><span
-                                                class="wc-block-product-categories-list-item-count"><span
-                                                    aria-hidden="true">4</span><span class="screen-reader-text">4
-                                                    products</span></span> </li>
+                                        <?php foreach($categories as $categorie) { ?>
+                                                <li class="wc-block-product-categories-list-item"> <a style
+                                                ><span
+                                                    class="wc-block-product-categories-list-item__name"><?= $categorie['Name'] ?></span></a><span
+                                                class="wc-block-product-categories-list-item-count"> </li>
+                                        <?php } ?>
                                     </ul>
                                 </div>
                             </div>
@@ -358,14 +309,9 @@
                             <div class="widget widget_fidalgo_core_social_links_group" data-area="shop-sidebar">
                                 <div class="qodef-social-links-group">
                                     <span class="qodef-link-holder">
-                                        <a itemprop="url" href="https://www.pinterest.com/qodeinteractive/"
-                                            target="_blank">Pinterest</a> </span>
+                                        <a itemprop="url">Facebook</a> </span>
                                     <span class="qodef-link-holder">
-                                        <a itemprop="url" href="https://www.facebook.com/QodeInteractive/"
-                                            target="_blank">Facebook</a> </span>
-                                    <span class="qodef-link-holder">
-                                        <a itemprop="url" href="https://www.instagram.com/qodeinteractive/"
-                                            target="_blank">Instagram</a> </span>
+                                        <a itemprop="url">Instagram</a> </span>
                                 </div>
                             </div>
                             <div class="widget widget_fidalgo_core_separator" data-area="shop-sidebar">
@@ -403,68 +349,6 @@
                                         style="border-style: solid;border-bottom-width: 0px;margin-top: 0px;margin-bottom: 22px">
                                     </div>
                                 </div>
-                            </div>
-                            <div class="widget widget_fidalgo_core_title_widget" data-area="shop-sidebar">
-                                <h4 class="qodef-widget-title" style="margin-bottom: 10px">
-                                    search </h4>
-                            </div>
-                            <div class="widget widget_block widget_search" data-area="shop-sidebar">
-                                <form role="search" method="get"
-                                    class="wp-block-search__button-inside wp-block-search__icon-button qodef-search-form wp-block-search"
-                                    action="https://fidalgo.qodeinteractive.com/"><label for="qodef-search-form-1"
-                                        class="qodef-search-form-label screen-reader-text">Search</label>
-                                    <div class="qodef-search-form-inner "><input type="search" id="qodef-search-form-1"
-                                            class="qodef-search-form-field " name="s" value placeholder="Search"
-                                            required /><button type="submit"
-                                            class="qodef-search-form-button  qodef--button-inside qodef--has-icon"><svg
-                                                class="qodef-svg--search" xmlns="http://www.w3.org/2000/svg"
-                                                width="19.811" height="19.811" viewBox="0 0 19.811 19.811">
-                                                <g fill="none" stroke="currentColor" stroke-linecap="round"
-                                                    stroke-linejoin="round" stroke-width="1.5"
-                                                    transform="translate(-2.25 -2.25)">
-                                                    <circle cx="8" cy="8" r="8" transform="translate(3 3)" />
-                                                    <path d="m21 21-4.35-4.35" />
-                                                </g>
-                                            </svg></button></div>
-                                </form>
-                            </div>
-                            <div class="widget widget_fidalgo_core_separator" data-area="shop-sidebar">
-                                <div
-                                    class="qodef-shortcode qodef-m  qodef-separator clear  qodef-border--solid qodef-show--yes qodef-show-1600--yes qodef-show-1512--yes qodef-show-1368--yes qodef-show-1200--yes qodef-show-1024--yes qodef-show-880--yes qodef-show-680--yes">
-                                    <div class="qodef-m-line"
-                                        style="border-style: solid;border-bottom-width: 0px;margin-top: 0px;margin-bottom: 25px">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="widget widget_fidalgo_core_title_widget" data-area="shop-sidebar">
-                                <h4 class="qodef-widget-title" style="margin-bottom: 15px">
-                                    tag </h4>
-                            </div>
-                            <div class="widget widget_block widget_tag_cloud" data-area="shop-sidebar">
-                                <p class="wp-block-tag-cloud"><a
-                                        href="https://fidalgo.qodeinteractive.com/product-tag/caramel/"
-                                        class="tag-cloud-link tag-link-21 tag-link-position-1"
-                                        style="font-size: 19.021276595745pt;" aria-label="caramel (6 items)">caramel</a>
-                                    <a href="https://fidalgo.qodeinteractive.com/product-tag/chocolate/"
-                                        class="tag-cloud-link tag-link-22 tag-link-position-2"
-                                        style="font-size: 20.510638297872pt;"
-                                        aria-label="chocolate (7 items)">chocolate</a>
-                                    <a href="https://fidalgo.qodeinteractive.com/product-tag/maki-rolls/"
-                                        class="tag-cloud-link tag-link-31 tag-link-position-3" style="font-size: 8pt;"
-                                        aria-label="maki rolls (2 items)">maki rolls</a>
-                                    <a href="https://fidalgo.qodeinteractive.com/product-tag/nigiri/"
-                                        class="tag-cloud-link tag-link-29 tag-link-position-4"
-                                        style="font-size: 11.574468085106pt;" aria-label="nigiri (3 items)">nigiri</a>
-                                    <a href="https://fidalgo.qodeinteractive.com/product-tag/pralines/"
-                                        class="tag-cloud-link tag-link-24 tag-link-position-5" style="font-size: 22pt;"
-                                        aria-label="pralines (8 items)">pralines</a>
-                                    <a href="https://fidalgo.qodeinteractive.com/product-tag/salmon/"
-                                        class="tag-cloud-link tag-link-30 tag-link-position-6"
-                                        style="font-size: 14.553191489362pt;" aria-label="salmon (4 items)">salmon</a>
-                                    <a href="https://fidalgo.qodeinteractive.com/product-tag/sweet/"
-                                        class="tag-cloud-link tag-link-25 tag-link-position-7" style="font-size: 22pt;"
-                                        aria-label="sweet (8 items)">sweet</a>
-                                </p>
                             </div>
                             <div class="widget widget_block widget_text" data-area="shop-sidebar">
                                 <p></p>
