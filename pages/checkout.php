a61ab10157a8a37442c991214f82999909ac2659
Yash Sen <yashsen0402@gmail.com>
2024-08-27 22:54:12 +0530

    User Side Checkout Completed

--- pages/checkout.php
+++ pages/checkout.php
@@ -1,121 +1,154 @@
 <?php
-	require '../includes/init.php';
-	include pathOf('includes/header.php');
-	include pathOf('includes/navbar.php');
+require '../includes/init.php';
+include pathOf('includes/header.php');
+include pathOf('includes/navbar.php');
 ?>
+
 <body class="page-template-default page page-id-9 theme-fidalgo qi-blocks-1.2.6 qodef-gutenberg--no-touch qode-framework-1.2.2 woocommerce-cart woocommerce-page woocommerce-no-js qodef-qi--no-touch qi-addons-for-elementor-1.6.9 qi-watermark-1.4.9 qodef-back-to-top--enabled qodef-skin--reseda-green  qodef-header--standard qodef-header-appearance--none qodef-mobile-header--standard qodef-drop-down-second--full-width qodef-drop-down-second--default fidalgo-core-1.0 fidalgo-1.0 qodef-content-grid-1400 qodef-header-standard--center qodef-search--fullscreen elementor-default elementor-kit-4" itemscope itemtype="https://schema.org/WebPage">
 
-<div id="qodef-page-outer">
-<div class="qodef-page-title qodef-m qodef-title--breadcrumbs qodef-alignment--left qodef-vertical-alignment--header-bottom qodef--has-image">
-<div class="qodef-m-inner">
-<div class="qodef-m-content qodef-content-grid ">
-<div itemprop="breadcrumb" class="qodef-breadcrumbs"><a itemprop="url" class="qodef-breadcrumbs-link" href="https://fidalgo.qodeinteractive.com/"><span itemprop="title">Home</span></a><span class="qodef-breadcrumbs-separator"></span><span itemprop="title" class="qodef-breadcrumbs-current">Cart</span></div></div>
-</div>
-</div>
-<div id="qodef-page-inner" class="qodef-content-grid">
-<main id="qodef-page-content" class="qodef-grid qodef-layout--template qodef-grid-template--12 " role="main">
-<div class="qodef-grid-inner">
-<div class="qodef-grid-item qodef-page-content-section qodef-col--content">
-<div class="woocommerce"><div class="woocommerce-notices-wrapper"></div><div class="wc-empty-cart-message">
-<div class="cart-empty woocommerce-info">
-Your cart is currently empty. </div>
-</div> <p class="return-to-shop">
-<a class="button wc-backward" href="https://fidalgo.qodeinteractive.com/shop/">
-Return to shop </a>
-</p>
-</div>
-</div>
-</div>
-</main>
-</div>
-</div>
-<footer id="qodef-page-footer" role="contentinfo">
-<div id="qodef-page-footer-top-area" class="qodef--has-appear">
-<div id="qodef-page-footer-top-area-inner" class="qodef-content-grid">
-<div class="qodef-grid qodef-layout--columns qodef-responsive--custom qodef-col-num--3 qodef-col-num--1024--3 qodef-col-num--880--1 qodef-col-num--680--1 qodef-alignment--center">
-<div class="qodef-grid-inner">
-<div class="qodef-grid-item">
-<div id="block-72" class="widget widget_block" data-area="qodef-footer-top-area-column-1">
-<div class="wp-block-group"><div class="wp-block-group__inner-container is-layout-constrained wp-block-group-is-layout-constrained">
-<h5 class="wp-block-heading">contact us</h5>
-<p style="font-family:jost; font-size:17px;">T. <a href="tel:123440567899">+12 344 0567899</a><br>
-M. <a href="/cdn-cgi/l/email-protection#0a6c636e6b666d654a6f726b677a666f24696567"><span class="__cf_email__" data-cfemail="40262924212c272f002538212d302c256e232f2d">[email&#160;protected]</span></a></p>
-</div></div>
-</div> </div>
-<div class="qodef-grid-item">
-<div id="block-73" class="widget widget_block" data-area="qodef-footer-top-area-column-2">
-<div class="wp-block-group"><div class="wp-block-group__inner-container is-layout-constrained wp-block-group-is-layout-constrained">
-<h5 class="wp-block-heading">address</h5>
-<a href="https://www.google.com/maps/place/P.za+della+Signoria,+12,+50122+Firenze+FI,+Italy/@43.7698405,11.254907,20z/data=!4m20!1m13!4m12!1m6!1m2!1s0x132a5400557fce11:0x77ef65e61406784a!2sP.za+della+Signoria,+12,+50122+Firenze+FI,+Italy!2m2!1d11.2552101!2d43.7698604!1m3!2m2!1d11.2552316!2d43.7698604!3e0!3m5!1s0x132a5400557fce11:0x77ef65e61406784a!8m2!3d43.7698604!4d11.2552101!16s%2Fg%2F11f3zphnxj?entry=ttu" target="_blank" rel="noopener">Piazza Della Signoria, 12 <br>21562 . Firenze . Italy</a>
-</div></div>
-</div> </div>
-<div class="qodef-grid-item">
-<div id="block-74" class="widget widget_block" data-area="qodef-footer-top-area-column-3">
-<div class="wp-block-group"><div class="wp-block-group__inner-container is-layout-constrained wp-block-group-is-layout-constrained">
-<h5 class="wp-block-heading">Opening hours</h5>
-<p>Everyday : From 12.30 To 23.00<br>Kitchen Closes At 22.00</p>
-</div></div>
-</div> </div>
-</div>
-</div>
-</div>
-</div>
-<div id="qodef-page-footer-bottom-area" class="qodef--has-appear">
-<div id="qodef-footer-logo-holder" class="qodef-content-full-width"><a href="https://fidalgo.qodeinteractive.com"><img width="384" height="137" src="https://fidalgo.qodeinteractive.com/wp-content/uploads/2023/09/logo-shop.png" class="attachment-full size-full" alt="a" decoding="async" loading="lazy" srcset="https://fidalgo.qodeinteractive.com/wp-content/uploads/2023/09/logo-shop.png 384w, https://fidalgo.qodeinteractive.com/wp-content/uploads/2023/09/logo-shop-300x107.png 300w" sizes="(max-width: 384px) 100vw, 384px" /></a></div> <div id="qodef-page-footer-bottom-area-inner" class="qodef-content-full-width">
-<div class="qodef-grid qodef-layout--columns qodef-responsive--custom qodef-col-num--2 qodef-col-num--880--1 qodef-col-num--680--1 qodef-alignment--space-between">
-<div class="qodef-grid-inner">
-<div class="qodef-grid-item">
-<div id="fidalgo_core_social_links_group-2" class="widget widget_fidalgo_core_social_links_group" data-area="qodef-footer-bottom-area-column-1"> <div class="qodef-social-links-group">
-<span class="qodef-link-holder">
-<a itemprop="url" href="https://www.pinterest.com/qodeinteractive/" target="_blank">PINTEREST</a> </span>
-<span class="qodef-link-holder">
-<a itemprop="url" href="https://www.facebook.com/QodeInteractive/" target="_blank">FACEBOOK</a> </span>
-<span class="qodef-link-holder">
-<a itemprop="url" href="https://www.instagram.com/qodeinteractive/" target="_blank">INSTAGRAM</a> </span>
-</div>
-</div> </div>
-<div class="qodef-grid-item">
-<div id="block-7" class="widget widget_block" data-area="qodef-footer-bottom-area-column-2"><p class="qodef-copyright" style="font-family:jost; font-size:11px; font-weight:400; color:#CED8D8; letter-spacing:2px; line-height:20px;">© 2024 <a href="https://qodeinteractive.com/" target="_blank" rel="nofollow noopener">QODE INTERACTIVE</a>, ALL RIGHTS RESERVED</p></div> </div>
-</div>
-</div>
-</div>
-</div>
-</footer>
-<a id="qodef-back-to-top" href="#" class="qodef--predefined">
-<span class="qodef-back-to-top-icon">
-<span class="qodef-back-to-top-inner">Top </span><span class="qodef-back-to-top-additional-border"></span> </span>
-</a>
-<div id="qodef-side-area">
-<a href="javascript:void(0)" id="qodef-side-area-close" class="qodef-opener-icon qodef-m qodef-source--predefined qodef--opened">
-<span class="qodef-m-icon ">
-<span class="qodef-m-lines"><span class="qodef-m-line qodef--1"></span><span class="qodef-m-line qodef--2"></span><span class="qodef-m-line qodef--3"></span><span class="qodef-m-line qodef--4"></span></span> </span>
-</a>
-<div id="qodef-side-area-inner">
-<div id="fidalgo_core_title_widget-19" class="widget widget_fidalgo_core_title_widget" data-area="side-area"> <h2 class="qodef-widget-title" style="margin-bottom: 35px">
-connect with us </h2>
-</div><div id="fidalgo_core_title_widget-15" class="widget widget_fidalgo_core_title_widget" data-area="side-area"> <h5 class="qodef-widget-title" style="margin-bottom: 10px">
-find us </h5>
-</div><div id="block-28" class="widget widget_block" data-area="side-area"><a href="https://www.google.com/maps/place/487+8th+Ave,+New+York,+NY+10001,+USA/@40.7528395,-73.995819,17z/data=!3m1!4b1!4m6!3m5!1s0x89c259adc1afce95:0x7b5d1d6a13472f6f!8m2!3d40.7528355!4d-73.9932441!16s%2Fg%2F11fk2p4skw?entry=ttu" target="_blank" rel="noopener">eight avenue 487, NY</a><br>
-<a href="tel:123344056">+123 344 056</a>, <a href="tel:123456789">+123 456 789</a></div><div id="fidalgo_core_title_widget-16" class="widget widget_fidalgo_core_title_widget" data-area="side-area"> <h5 class="qodef-widget-title" style="margin-bottom: 10px">
-contact us </h5>
-</div><div id="block-30" class="widget widget_block" data-area="side-area"><a href="/cdn-cgi/l/email-protection#97e5f2e4f2e5e1f6e3fef8f9e4d7f2eff6fae7fbf2b9f4f8fa"><span class="__cf_email__" data-cfemail="d7a5b2a4b2a5a1b6a3beb8b9a497b2afb6baa7bbb2f9b4b8ba">[email&#160;protected]</span></a>,<br>
-<a href="/cdn-cgi/l/email-protection#97f8f1f1fef4f2d7f2eff6fae7fbf2b9f4f8fa"><span class="__cf_email__" data-cfemail="432c25252a202603263b222e332f266d202c2e">[email&#160;protected]</span></a></div><div id="fidalgo_core_title_widget-17" class="widget widget_fidalgo_core_title_widget" data-area="side-area"> <h5 class="qodef-widget-title" style="margin-bottom: 10px">
-opening hours </h5>
-</div><div id="block-32" class="widget widget_block widget_text" data-area="side-area">
-<p>Tuesday - thursday 4pm - 11pm<br>Friday &amp; saturday 12pm till late</p>
-</div><div id="block-67" class="widget widget_block" data-area="side-area">
-<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
-</div><div id="fidalgo_core_social_links_group-4" class="widget widget_fidalgo_core_social_links_group" data-area="side-area"> <div class="qodef-social-links-group">
-<span class="qodef-link-holder">
-<a itemprop="url" href="https://www.pinterest.com/qodeinteractive/" target="_blank">Pinterest</a> </span>
-<span class="qodef-link-holder">
-<a itemprop="url" href="https://www.facebook.com/QodeInteractive/" target="_blank">Facebook</a> </span>
-<span class="qodef-link-holder">
-<a itemprop="url" href="https://www.instagram.com/qodeinteractive/" target="_blank">Instagram</a> </span>
-</div>
-</div> </div>
-</div>
-</div>
-<?php
+	<div id="qodef-page-inner" class="qodef-content-grid">
+		<main id="qodef-page-content" class="qodef-grid qodef-layout--template qodef-grid-template--12 " role="main">
+			<div class="qodef-grid-inner">
+				<div class="qodef-grid-item qodef-page-content-section qodef-col--content">
+					<div class="woocommerce">
+						<div class="woocommerce-notices-wrapper"></div>
+						<div id="qodef-woo-page" class="qodef--checkout">
+							<div class="woocommerce-form-coupon-toggle">
+								<div class="woocommerce-info">
+									Have a coupon? <a href="#" class="showcoupon">Click here to enter your code</a> </div>
+							</div>
+							<form class="checkout_coupon woocommerce-form-coupon" method="post" style="display:none">
+								<p>If you have a coupon code, please apply it below.</p>
+								<p class="form-row form-row-first">
+									<label for="coupon_code" class="screen-reader-text">Coupon:</label>
+									<input type="text" name="coupon_code" class="input-text" placeholder="Coupon code" id="coupon_code" value="">
+								</p>
+								<p class="form-row form-row-last">
+									<button type="submit" class="button" name="apply_coupon" value="Apply coupon">Apply coupon</button>
+								</p>
+								<div class="clear"></div>
+							</form>
+							<div class="woocommerce-notices-wrapper"></div>
+								<div class="col2-set" id="customer_details">
+									<div class="col-1">
+										<div class="woocommerce-billing-fields">
+											<h3>Billing details</h3>
+											<div class="woocommerce-billing-fields__field-wrapper">
+												<p class=""><label>First name&nbsp;<abbr class="required" title="required">*</abbr></label><input type="text" class="input-text " name="FirstName" id="FirstName"></p>
+												<p class=""><label>Last name&nbsp;<abbr class="required" title="required">*</abbr></label><input type="text" class="input-text " name="LastName" id="LastName"></p>
+												<p class=""><label>Street address&nbsp;<abbr class="required" title="required">*</abbr></label><input type="text" class="input-text " name="Address" id="Address" placeholder="House number and street name" value="" autocomplete="address-line1" data-placeholder="House number and street name"></p>
+												<p class=""><label>Town / City&nbsp;<abbr class="required" title="required">*</abbr></label><input type="text" class="input-text " name="City" id="City"></p>
+												<p class=""><label>ZIP Code&nbsp;<abbr class="required" title="required">*</abbr></label><input type="number" class="input-text " name="ZipCode" id="ZipCode"></p>
+												<p class=""><label>Phone&nbsp;<abbr class="required" title="required">*</abbr></label><input type="number" class="input-text " name="Phone" id="Phone"></p>
+												<p class=""><label>Email address&nbsp;<abbr class="required" title="required">*</abbr></label><input type="email" class="input-text " name="Email" id="Email"></p>
+											</div>
+										</div>
+									</div>
+								</div>
+								<h3 id="order_review_heading">Your order</h3>
+								<div id="order_review" class="">
+									<table class="shop_table woocommerce-checkout-review-order-table">
+										<thead>
+											<tr>
+												<th class="product-name">Product</th>
+												<th class="product-total">Subtotal</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr class="cart_item">
+												<td class="product-name">
+													Pecan Sandies&nbsp; <strong class="product-quantity">×&nbsp;1</strong> </td>
+												<td class="product-total">
+													<span class="woocommerce-Price-amount amount"><bdi><span class="woocommerce-Price-currencySymbol">$</span>27.00</bdi></span>
+												</td>
+											</tr>
+											<tr class="cart_item">
+												<td class="product-name">
+													Whiskey Cream&nbsp; <strong class="product-quantity">×&nbsp;1</strong> </td>
+												<td class="product-total">
+													<span class="woocommerce-Price-amount amount"><bdi><span class="woocommerce-Price-currencySymbol">$</span>30.00</bdi></span>
+												</td>
+											</tr>
+										</tbody>
+										<tfoot>
+											<tr class="cart-subtotal">
+												<th>Subtotal</th>
+												<td><span class="woocommerce-Price-amount amount"><bdi><span class="woocommerce-Price-currencySymbol">$</span>57.00</bdi></span></td>
+											</tr>
+											<tr class="woocommerce-shipping-totals shipping">
+												<th>Shipping</th>
+												<td data-title="Shipping">
+													<ul id="shipping_method" class="woocommerce-shipping-methods">
+													</ul>
+												</td>
+											</tr>
+											<tr class="order-total">
+												<th>Total</th>
+												<td><strong><span class="woocommerce-Price-amount amount"><bdi><span class="woocommerce-Price-currencySymbol">$</span>57.00</bdi></span></strong> </td>
+											</tr>
+										</tfoot>
+									</table> 
+									<div id="payment" class="t">
+										<ul class="wc_payment_methods payment_methods methods">
+											<li class="wc_payment_method payment_method_bacs">
+												<input id="payment_method_bacs" type="radio" class="input-radio" name="payment_method" value="bacs" checked="checked" data-order_button_text="">
+												<label for="payment_method_bacs">
+													Direct bank transfer </label>
+												<div class="payment_box payment_method_bacs">
+													<p>Make your payment directly into our bank account. Please use your Order ID as the payment reference. Your order will not be shipped until the funds have cleared in our account.</p>
+												</div>
+											</li>
+											<li class="wc_payment_method payment_method_cod">
+												<input id="payment_method_cod" type="radio" class="input-radio" name="payment_method" value="cod" data-order_button_text="">
+
+												<label for="payment_method_cod">
+													Cash on delivery </label>
+												<div class="payment_box payment_method_cod" style="display:none;">
+													<p>Pay with cash upon delivery.</p>
+												</div>
+											</li>
+										</ul> 
+										<div class="">
+											<button onclick="checkout()">Place order</button>
+										</div>
+									</div>
+								</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+	</div>
+	<script>
+		function checkout() {
+			var FirstName = $("#FirstName").val();
+			var LastName = $("#LastName").val();
+			var Address = $("#Address").val();
+			var City = $("#City").val();
+			var ZipCode = $("#ZipCode").val();
+			var Phone = $("#Phone").val();
+			var Email = $("#Email").val();
+
+			$.ajax({
+				type: "POST",
+				url: "../admin/api/checkout/insert.php",
+				data: {
+					FirstName: FirstName,
+					LastName: LastName,
+					Address: Address,
+					City: City,
+					ZipCode: ZipCode,
+					Phone: Phone,
+					Email: Email
+				},
+				success: function(data) {
+					alert(data);
+				}
+			});
+		}
+	</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+	<?php
 	include pathOf('includes/scripts.php');
-?>
+	include pathOf('includes/footer.php');
+	?>
